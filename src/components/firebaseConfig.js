2d88d049180662ec579a9976c7b27f9bdfb11005
David Owczarz <owcdaw@gmail.com>
2020-02-23 20:55:35 +0100

    Firebase configured

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+	apiKey: process.env.VUE_APP_APIKEY,
+    authDomain: process.env.VUE_APP_AUTHDOMAIN,
+    databaseURL: process.env.VUE_APP_DBURL,
+    projectId: process.env.VUE_APP_PROJECTID,
+    storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+    appId: process.env.VUE_APP_APPID,
+    measurementId: process.env.VUE_APP_MEASUREMENTID
+}
---
+++ src/components/firebaseInit.js
@@ -0,0 +1,8 @@
+import firebase from 'firebase';
+import 'firebase/firestore';
+import firebaseConfig from './firebaseConfig';
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+export default firebase.firestore();
